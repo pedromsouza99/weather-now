4d2fbdbd75c7e73d7215c0e1cd8b646c38f27510
pedromsouza99 <pedromsouza99@gmail.com>
2022-07-09 12:32:31 -0300

    feat: adds style theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
 import { Home } from "./pages/Home/Home";
@@ -7,11 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+root.render(<Home />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

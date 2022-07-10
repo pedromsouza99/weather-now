e5f33a01352fdbf01cf944803695fddbd23a4be0
pedromsouza99 <pedromsouza99@gmail.com>
2022-07-10 12:01:28 -0300

    feat: adds Loader

---
+++ src/components/layout/Loader.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import loadersvg from "../../assets/loader.svg";
+interface LoaderProps {
+  className?: string;
+}
+
+export function Loader(props: LoaderProps) {
+  return (
+    <LoaderStyle {...props}>
+      <img src={loadersvg} alt=" " />
+    </LoaderStyle>
+  );
+}
+
+const LoaderStyle = styled.div`
+  display: inline-block;
+`;
